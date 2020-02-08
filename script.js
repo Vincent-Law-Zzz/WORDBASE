a1eeecaa130b06f2aa83ae41b2515b138d95d51c
Alexander Bonchev <alexander.b@electroneek.com>
2020-02-08 19:00:26 +0300

    Added yandex translater

--- script.js
+++ script.js
@@ -18,4 +18,28 @@
         
 
     } );
+
+    var text = "My dog"
+   
+    var yatr ={
+        key:"trnsl.1.1.20200208T134436Z.1f4d7b6646b15486.af12164c15416a7c32cb788f5dc3a3f57b9f8e06",
+        url:"https://translate.yandex.net/api/v1.5/tr.json/translate",
+        
+        
+        ajaxGet : function( text , callback ) {
+        var request = new XMLHttpRequest();
+        request.open('GET',`${this.url}?lang=en-ru&key=${this.key}&text=${text}`,callback );
+        request.send();
+        request.onreadystatechange = function(){
+            if( request.readyState == 4 && request.status == 200){
+                var req = request.responseText;
+                req = JSON.parse(req);
+                req = req.text;
+                alert(req);
+            }
+        };
+        request.onreadystatechange();
+        }
+   }
+   yatr.ajaxGet(text,yatr.request);
    
